f93a37bfbd7296c1b4b71efd238d5693c07b4167
rictorlome <rictorlome@gmail.com>
2018-03-26 17:11:02 -0400

    Double background, fix seam

--- dist/bundle.js
+++ dist/bundle.js
@@ -311,16 +311,18 @@ __webpack_require__.r(__webpack_exports__);
 class Background {
   constructor() {
     this.img = new Image(2500, 10000);
-    this.img.src = 'assets/whale-olympics-background.png';
+    this.img.src = 'assets/whale-olympics-background2.png';
   }
 
   draw(ctx, whale) {
     const img = this.img;
-    let sx = whale.pos[0];
+    let sx = whale.pos[0] % 5000;
     let sy = whale.pos[1];
-
-    ctx.drawImage(img, (sx - 1925) % img.width, sy, ctx.canvas.width, ctx.canvas.height, 0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(img, sx % img.width, sy, ctx.canvas.width, ctx.canvas.height, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    let whiteSpace = 5000 - whale.pos[0] % 5000;
+    ctx.drawImage(img, sx, sy, ctx.canvas.width, ctx.canvas.height, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    if (whiteSpace > 0 && whiteSpace < 575) {
+      ctx.drawImage(img, 0, sy, ctx.canvas.width, ctx.canvas.height, whiteSpace, 0, ctx.canvas.width, ctx.canvas.height);
+    }
   }
 }
 
